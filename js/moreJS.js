d561d997dac25920dd58fc8dad0e092cc8f7171b
Rina45 <90148827+Rina45@users.noreply.github.com>
2022-01-26 23:00:47 +0200

    and writeDays and writeHemonth

--- js/moreJS.js
+++ js/moreJS.js
@@ -1,17 +1,3 @@
-function writeTitle(dateH, dateL) {
-    let heMonth = document.getElementById("heMonth");
-    heMonth.textContent = writeHeMonth(dateH);
-    let loMonth = document.getElementById("loMonth");
-    loMonth.textContent = writeLoMonth(dateL);
-}
-
-function writeDays(dateH, dateL) {
-    let calender = document.getElementById(tbody);
-    for (let day = 1; day <= daysH; day++) {
-        createDay(day); // day is dateH object
-    }
-}
-
 function createDay(day) {
     let day = document.createElement('div');
     day.className = 'td';
@@ -19,29 +5,10 @@ function createDay(day) {
     datePlace.className = 'date';
     let ot = document.createElement('div');
     ot.className = 'ot';
-    ot.textContent = numToOt(day);
+    ot.textContent = numToHe(day);
     let num = document.createElement('div');
     num.className = 'num';
     datePlace.append(ot, num);
     day.append(datePlace);
-    return
-}
-
-async function getMonth(firstDate, numDays = 29) {
-    const lastDate = firstDate.setDate(firstDate.getDate() + numDays);
-    const url = `https://www.hebcal.com/converter?cfg=json&start=${stringDate(firstDate)}&end=${stringDate(lastDate)}&g2h=1`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
-}
-
-function stringDate(date = new Date()) {
-    return date.toISOString().slice(0, 10);
-}
-
-async function getLDate(dateH) {
-    const url = `https://www.hebcal.com/converter?cfg=json&hy=${dateH.hy}&hm=${dateH.hm}&hd=${dateH.hd}&h2g=1`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data;
+    return day;
 }
--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 //make object for a date square {ot, num, events array, shabbat parashah}
-
-
 /* steps for the js script:
-1.find the first day in the current monthH (=hebrew month)
-2.change the caption to fit the current month (the monthL (=none hebrew month) of the first and last day of the month)
+
 3.make the days (including ot, num and Parashah on shabbat)
 4.how to make the today date with the id today.
 5.add holidays (option to others events in the future) 
@@ -14,33 +11,184 @@ let dateH = {};
 
 async function firstRender() {
     dateH = await getHebrewDate(dateL);
-    dateL = await getLDate({ ...dateH, hd: 1 });
-    renderCalender(dateL);
+    dateH.hd = 1;
+    dateL = await getLoDate(dateH);
+    renderCalender(dateL, dateH);
 }
 
 window.onload = firstRender();
 
-function renderCalender(dateL) {
+function renderCalender(dateL, dateH) {
+    writeTitle(dateH, dateL);
+    writeDays(dateL);
+}
+
+function writeTitle(dateH, dateL) {
     let heMonth = document.getElementById("heMonth");
-    getHebrewDate(dateL).then(date => heMonth.textContent = date.hebrew);
+    heMonth.textContent = writeHeMonth(dateH);
     let loMonth = document.getElementById("loMonth");
     loMonth.textContent = writeLoMonth(dateL);
 }
 
-const monthsL = ['ינואר',
-    'פברואר',
-    'מרץ',
-    'אפריל',
-    'מאי',
-    'יוני',
-    'יולי',
-    'אוגוסט',
-    'ספטמבר',
-    'אוקטובר',
-    'נובמבר',
-    'דצמבר'];
+function writeHeMonth(date) {
+    const heMonths = [
+        "תשרי",
+        "חשון",
+        "כסלו",
+        "טבת",
+        "שבט",
+        "אדר",
+        "אדר א'",
+        "אדר ב'",
+        "ניסן",
+        "אייר",
+        "סיון",
+        "תמוז",
+        "אב",
+        "אלול"];
+    const enMonths = [
+        'Tishrei',
+        'Cheshvan',
+        'Kislev',
+        'Tevet',
+        'Sh\'vat',
+        'Adar',
+        'Adar I',
+        'Adar II',
+        'Nisan',
+        'Iyyar',
+        'Sivan',
+        'Tamuz',
+        'Av',
+        'Elul'];
+    const month = heMonths[enMonths.indexOf(date.hm)];
+    const year = numToHe(date.hy);
+    return `${month} ${year}`;
+}
+
+function numToHe(num) {
+    let word = '';
+    if (num >= 1000) {
+        word = numToHe(Math.floor(num / 1000)) + "'";
+        num %= 1000
+    }
+    do {
+        switch (true) {
+            case num >= 400:
+                word += 'ת';
+                num -= 400;
+                break;
+            case num >= 300:
+                word += 'ש';
+                num -= 300;
+                break;
+            case num >= 200:
+                word += 'ר';
+                num -= 200;
+                break;
+            case num >= 100:
+                word += 'ק';
+                num -= 100;
+                break;
+            case num >= 90:
+                word += 'צ';
+                num -= 90;
+                break;
+            case num >= 80:
+                word += 'פ';
+                num -= 80;
+                break;
+            case num >= 70:
+                word += 'ע';
+                num -= 70;
+                break;
+            case num >= 60:
+                word += 'ס';
+                num -= 60;
+                break;
+            case num >= 50:
+                word += 'נ';
+                num -= 50;
+                break;
+            case num >= 40:
+                word += 'מ';
+                num -= 40;
+                break;
+            case num >= 30:
+                word += 'ל';
+                num -= 30;
+                break;
+            case num >= 20:
+                word += 'כ';
+                num -= 20;
+                break;
+            case num >= 10:
+                word += 'י';
+                num -= 10;
+                break;
+            case num === 9:
+                word += 'ט';
+                num -= 9;
+                break;
+            case num === 8:
+                word += 'ח';
+                num -= 8;
+                break;
+            case num === 7:
+                word += 'ז';
+                num -= 7;
+                break;
+            case num === 6:
+                word += 'ו';
+                num -= 6;
+                break;
+            case num === 5:
+                word += 'ה';
+                num -= 5;
+                break;
+            case num === 4:
+                word += 'ד';
+                num -= 4;
+                break;
+            case num === 3:
+                word += 'ג';
+                num -= 3;
+                break;
+            case num === 2:
+                word += 'ב';
+                num -= 2;
+                break;
+            case num === 1:
+                word += 'א';
+                num -= 1;
+                break;
+            default:
+                break;
+        }
+    } while (num > 0);
+    if (word.length >= 2 && word[-2] !== "'") {
+        let char = word.slice(-1);
+        word = word.slice(0, -1)
+        word += '"' + char;
+    }
+    return word;
+}
 
 function writeLoMonth(dateL) {
+
+    const monthsL = ['ינואר',
+        'פברואר',
+        'מרץ',
+        'אפריל',
+        'מאי',
+        'יוני',
+        'יולי',
+        'אוגוסט',
+        'ספטמבר',
+        'אוקטובר',
+        'נובמבר',
+        'דצמבר'];
+
     if (dateL.getMonth() !== 11) {
         return monthsL[dateL.getMonth()] + " - " + monthsL[dateL.getMonth() + 1] + " " + dateL.getFullYear();
     }
@@ -50,7 +198,31 @@ function writeLoMonth(dateL) {
     }
 }
 
-//DON'T FORGET TO ADD HEBREW DATES + -
+function writeDays(dateL) {
+    const month = getMonth(dateL).result;
+    console.log(month);
+    let calender = document.getElementById('tbody');
+    for (let numDay = 0; numDay < 30; numDay++) {
+        calender.append(createDay(numDay));
+        console.log(month[numDay]);
+    }
+}
+
+function createDay(date) {
+    let day = document.createElement('div');
+    day.className = 'td';
+    let datePlace = document.createElement('div');
+    datePlace.className = 'date';
+    let ot = document.createElement('div');
+    ot.className = 'ot';
+    ot.textContent = numToHe(day);
+    let num = document.createElement('div');
+    num.className = 'num';
+    num.textContent = date;
+    datePlace.append(ot, num);
+    day.append(datePlace);
+    return day;
+}
 
 document.querySelector("#next > .month").addEventListener('click', nextMonth);
 document.querySelector("#next > .year").addEventListener('click', nextYear);
@@ -59,59 +231,17 @@ document.querySelector("#last > .year").addEventListener('click', lastYear);
 
 function nextMonth() {
     dateL.setMonth(dateL.getMonth() + 1);
-    // dateH = checkDateH();
-    renderCalender(dateL);
+    renderCalender(dateL, dateH);
 }
 
-function lastMonth() {
-    dateL.setMonth(dateL.getMonth() - 1);
-    // dateH = checkDateH();
-    renderCalender(dateL);
+async function lastMonth() {
+    dateH = preMonth(dateH);
+    dateL = await getLoDate(dateH);
+    renderCalender(dateL, dateH);
 }
 
-function nextYear() {
-    dateL.setFullYear(dateL.getFullYear() + 1);
-    // dateH = checkDateH();
-    renderCalender(dateL);
-}
-
-function lastYear() {
-    dateL.setFullYear(dateL.getFullYear() - 1);
-    // dateH = checkDateH();
-    renderCalender(dateL);
-}
-
-async function getHebrewDate(dateL) {
-    const url = `https://www.hebcal.com/converter?cfg=json&gy=${dateL.getFullYear()}&gm=${dateL.getMonth() + 1}&gd=${dateL.getDate()}&g2h=1`;
-    const response = await fetch(url);
-    const data = await response.json();
-    return data
-}
-
-async function getLDate(dateH) {
-    const url = `https://www.hebcal.com/converter?cfg=json&hy=${dateH.hy}&hm=${dateH.hm}&hd=${dateH.hd}&h2g=1`;
-    const response = await fetch(url);
-    const data = await response.json();
-    let date = new Date(data.gy, data.gm - 1, data.gd);
-    return date;
-}
-
-const MONTHS_H = {
-    Tishrei: "תשרי",
-    Cheshvan: "חשון",
-    Kislev: "כסלו",
-    Tevet: "טבת",
-    'Sh\'avt': "שבט",
-    Adar: "אדר",
-    'Adar I': "אדר א'",
-    'Adar II': "אדר ב'",
-    Nisan: "ניסן",
-    Iyyar: "אייר",
-    Sivan: "סיון",
-    Tamuz: "תמוז",
-    Av: "אב",
-    Elul: "אלול",
-    list: [
+function preMonth(date) {
+    const enMonths = [
         'Tishrei',
         'Cheshvan',
         'Kislev',
@@ -125,9 +255,6 @@ const MONTHS_H = {
         'Av',
         'Elul'
     ]
-}
-
-function preMonth(date) {
     switch (date.hm) {
         case 'Nisan':
             date.hm = 'Adar II';
@@ -143,8 +270,51 @@ function preMonth(date) {
             date.hy--;
             break;
         default:
-            date.hm = MONTHS_H.list[MONTHS_H.list.indexOf(date.hm) - 1];
+            date.hm = enMonths[enMonths.indexOf(date.hm) - 1];
             break;
     }
     return date;
+}
+
+async function nextYear() {
+    dateH.hy++;
+    dateL = await getLoDate(dateH);
+    renderCalender(dateL, dateH);
+}
+
+async function lastYear() {
+    dateH.hy--;
+    dateL = await getLoDate(dateH);
+    renderCalender(dateL, dateH);
+}
+
+async function getHebrewDate(dateL) {
+    const url = `https://www.hebcal.com/converter?cfg=json&gy=${dateL.getFullYear()}&gm=${dateL.getMonth() + 1}&gd=${dateL.getDate()}&g2h=1`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data
+}
+
+async function getLoDate(dateH) {
+    const url = `https://www.hebcal.com/converter?cfg=json&hy=${dateH.hy}&hm=${dateH.hm}&hd=${dateH.hd}&h2g=1`;
+    const response = await fetch(url);
+    const data = await response.json();
+    let date = new Date(data.gy, data.gm - 1, data.gd);
+    return date;
+}
+
+async function getMonth(firstDate, numDays = 30) {
+    let lastDate = new Date(firstDate);
+    lastDate.setDate(firstDate.getDate() + numDays);
+    const url = `https://www.hebcal.com/converter?cfg=json&start=${stringDate(firstDate)}&end=${stringDate(lastDate)}&g2h=1`;
+    const response = await fetch(url);
+    const data = await response.json();
+    const monthObj = await data.hdates;
+    const month = Object.entries(monthObj);
+    return month;
+}
+
+function stringDate(date) {
+    date.setDate(date.getDate() + 1);
+    return date.toISOString().slice(0, 10);
 }
